21ccf013ce9d6ddcd6e080adb0592fe3210d0615
alieniasty <adrianjago95@gmail.com>
2023-02-14 22:42:35 +0100

    Move automation samples to different repo

--- AutomationSamples/kv-migrate.ps1
+++
@@ -1,6 +0,0 @@
-$secretNames = (Get-AzKeyVaultSecret -VaultName $sourceVaultName).Name
-
-$secretNames.foreach{
-    Set-AzKeyVaultSecret -VaultName $destVaultName -Name $_ `
-        -SecretValue (Get-AzKeyVaultSecret -VaultName $sourceVaultName -Name $_).SecretValue
-}
